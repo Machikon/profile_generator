edbb32e285714016a6e0dcbe5237ddaca5e8940d
Machikon <106935371+Machikon@users.noreply.github.com>
2022-10-26 09:24:31 -0700

    added engineer and intern to js

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+const internal = require('stream');
 
 askQuestions(){
 
@@ -8,10 +9,11 @@ inquirer
     {
         type:'list',
         name:'employeeLevel',
-        choices:['Manager', 'Engineer', 'Intern', 'Quit'],
+        choices:['Manager', 'Engineer', 'Intern', 'Finish'],
         message:'Which level of employee so you want to add to your team?'
     }
 )
+//  ask questions about Manager 
 .then(({employeeLevel}) =>{
     if(employeeLevel === 'Manager'){
         inquirer.prompt([
@@ -43,5 +45,74 @@ const newManager = new Manager(answerMgr.name, answerMgr.id, answerMgr.email, an
 this.employeeArray.push(newManager);
 
 this.askQuestions();
+});
+
+// ask questions about engineer
+} else if (employeeLevel === 'Engineer'){
+    inquirer.prompt([
+        {
+            type:'input',
+            name:'name',
+            message:'What is the name of the engineer?',
+        },
+        {
+            type:'number',
+            name:'id',
+            message:'What is the ID of the engineer?',
+        },
+        {
+            type:'input',
+            name:'email',
+            message:'What is the email of the engineer?',
+        },
+        {
+            type:'input',
+            name:'github',
+            message:'What is the GitHub username of the engineer?',
+        }
+
+    ])
+
+.then((answerEgnr)=>{
+    const newEngineer = new Engineer(answerEgnr.name, answerEgnr.id, answerEgnr.email, answerEgnr.github);
+    this.employeeArray.push(newEngineer);
+    
+    this.askQuestions();
+});
+
+// ask questions about intern
+} else if (employeeLevel === 'Intern'){
+    inquirer.prompt([
+        {
+            type:'input',
+            name:'name',
+            message:'What is the name of the intern?',
+        },
+        {
+            type:'number',
+            name:'id',
+            message:'What is the ID of the intern?',
+        },
+        {
+            type:'input',
+            name:'email',
+            message:'What is the email of the intern?',
+        },
+        {
+            type:'input',
+            name:'school',
+            message:'What is the name of the school intern goes?',
+        }
+    ])
+.then((answerIntn) =>{
+    const newIntern = new internal(answerIntn.name, answerIntn.id, answerIntn.email, answerIntn.school);
+    this.employeeArray.push(newIntern);
+
+    this.askQuestions();
+});
+
+}else if (employeeLevel ==='Finish'){
+    fs.writeFile('./')
 }
-}
+
+
